091794d299cfc3c919ba852113d4035a123ede27
Jonathan Norman <jnorman2123@gmail.com>
2019-10-04 04:28:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,16 @@
 require 'pry'
 
 def consolidate_cart(cart)
-  binding.pry
+  final_hash = {}
+  cart.each do|element_hash| 
+    element_name = element_hash.keys[0]
+    element_stats = element_hash.values[0]
+    
+    if final_hash.has_key?(element_name)
+      element_stats[:count] += 1 
+    else 
+      final_hash[element_stats][:count] = 1 
+    end 
+  end 
 end 
 
