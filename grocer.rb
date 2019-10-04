c040f2cf06b8d4b770dc6bcea01c62a534c83a7b
Jonathan Norman <jnorman2123@gmail.com>
2019-10-04 05:40:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,7 +48,7 @@ def checkout(cart, coupons)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_discount = apply_clearance(applied_coupons)
   total = applied_discount.reduce(0) { |acc, (key, value)| acc += (value[:count] * value[:price])}
-  total > 100 ? 
+  total > 100 ? total * 0.9 : total 
 end 
 
 
